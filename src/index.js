3e7e51ed8e4660a89ee4238fb4654a41581aafd9
Francisco Zuniga <fzuniga325@gmail.com>
2023-11-19 23:27:23 -0600

    finished routes

--- src/index.js
+++ src/index.js
@@ -1,39 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-
-//pages
-import VendingMachine from './pages/VendingMachine';
 import Cakes from './pages/Cakes';
 import Nuts from './pages/Nuts';
 import Gum from './pages/Gum';
 import Soda from './pages/Soda';
 import Chips from './pages/Chips';
 import Candy from './pages/Candy';
+import Error from './pages/Error';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />} >
-      <Route index element={<VendingMachine />} />
-      <Route path="cakes" element={<Cakes />} />
-      <Route path="nuts" element={<Nuts />} />
-      <Route path="gum" element={<Gum />} />
-      <Route path="soda" element={<Soda />} />
-      <Route path="chips" element={<Chips />} />
-      <Route path="candy" element={<Candy />} />
-    </Route>
-  )
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+const Root = () => (
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="cakes" element={<Cakes />} />
+        <Route path="nuts" element={<Nuts />} />
+        <Route path="gum" element={<Gum />} />
+        <Route path="soda" element={<Soda />} />
+        <Route path="chips" element={<Chips />} />
+        <Route path="candy" element={<Candy />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
